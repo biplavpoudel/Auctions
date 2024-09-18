fdd957158e841071e1e9b8574874a45eacc146ef
biplavpoudel <biplavpoudel764@gmail.com>
2024-09-18 22:17:53 +0545

    Added pagination and search functionality

---
+++ Auctions/PaginatedList.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Auctions;
+
+public class PaginatedList<T>: List<T>
+{
+    public int PageIndex { get; private set; }
+    public int TotalPages { get; private set; }
+
+    public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+    {
+        PageIndex = pageIndex;
+        TotalPages = (int)Math.Ceiling(count/(double)pageSize);
+        this.AddRange(items);
+    }
+
+    public bool HasPreviousPage => PageIndex > 1;
+    public bool HasNextPage => PageIndex < TotalPages;
+
+    public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int PageSize)
+    {
+        var count = await source.CountAsync();
+        var items = await source.Skip((pageIndex-1)*PageSize).Take(PageSize).ToListAsync();
+        return new PaginatedList<T>(items, count, pageIndex, PageSize);
+    }
+}
