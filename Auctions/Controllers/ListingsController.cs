bb32b8517a48e3f0b911010b9a7086c6603900e1
biplavpoudel <biplavpoudel764@gmail.com>
2024-09-19 13:54:53 +0545

    added mylistings section to see all listings, sold or not!

--- Auctions/Controllers/ListingsController.cs
+++ Auctions/Controllers/ListingsController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Auctions.Data;
 using Auctions.Models;
 using Auctions.Data.Services;
+using System.Security.Claims;
 
 namespace Auctions.Controllers
 {
@@ -37,7 +38,15 @@ namespace Auctions.Controllers
                 applicationDbContext = applicationDbContext.Where(a => a.Title.Contains(searchString));
                 return View(await PaginatedList<Listing>.CreateAsync(applicationDbContext.Where(l => l.IsSold == false).AsNoTracking(), pageNumber ?? 1, pageSize));
             }
-            return View(await PaginatedList<Listing>.CreateAsync(applicationDbContext./*Where(l => l.IsSold == false).*/AsNoTracking(), pageNumber ??1, pageSize));
+            return View(await PaginatedList<Listing>.CreateAsync(applicationDbContext.Where(l => l.IsSold == false).AsNoTracking(), pageNumber ??1, pageSize));
+
+        }
+
+        public async Task<IActionResult> MyListings(int? pageNumber)
+        {
+            var applicationDbContext = _listingsService.GetAll();
+            int pageSize = 3;
+            return View("Index", await PaginatedList<Listing>.CreateAsync(applicationDbContext.Where(l => l.IdentityUserId == User.FindFirstValue(ClaimTypes.NameIdentifier)).AsNoTracking(), pageNumber ?? 1, pageSize));
 
         }
 
